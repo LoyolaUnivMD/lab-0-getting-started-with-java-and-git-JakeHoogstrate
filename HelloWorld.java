a52f99f971e9aace8104a50a254875165bbfb567
jakehoogstrate <jakehoogstrate5@gmail.com>
2024-01-17 18:03:56 -0500

    finished code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,17 @@
-//HelloWorld.java
+// Programmer: Jake Hoogstrate
+// Course:  CS 212
+// Due Date: 1/19/24
+// Lab Assignment: 0
+// Problem Statement:  Output programmers’ initial
+// Input: None
+// Output:  The initials of the programmers in ASCII art form
+// Credits: None
 
-class HelloWorld {
-    public static void main(String[] args) {
-
-        System.out.println("Hello, World!");
-        
-    }
-}
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("   ╭┳╮ ╭╮");
+        System.out.println("   ┃┃╰━╯┃");
+        System.out.println(" ╭╮┃┃╭━╮┃");
+        System.out.println(" ┃╰╯┃┃ ┃┃");
+        System.out.println(" ╰━━┻╯ ╰╯");
+    }}
